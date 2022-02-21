16ffa27db9f75f847eb9bc12d25f2a05874fd708
Schirrms <48877593+Schirrms@users.noreply.github.com>
2022-02-21 16:59:23 +0100

    Initial project Upload (v 0.0.3)

---
+++ schirrms-location-code/en.dict.schirrms-location-code.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2020 M. Camara
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('EN US', 'English', 'English', array(
+	// Dictionary entries go here
+	'Class:Location/Attribute:locationcode' => 'Location Code',
+	'Class:Location/Attribute:locationcode+' => 'Location Code used for data export.',
+	'Class:PhysicalDevice/Attribute:locationcode' => 'Location Code',
+	'Class:VirtualMachine/Attribute:locationcode' => 'Location Code',
+));
+?>
---
+++ schirrms-location-code/fr.dict.schirrms-location-code.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2020 M. Camara
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'Class:Location/Attribute:locationcode' => 'Code du site',
+	'Class:Location/Attribute:locationcode+' => 'Code du site utilisé pour les exports.',
+	'Class:PhysicalDevice/Attribute:locationcode' => 'Code du site',
+	'Class:VirtualMachine/Attribute:locationcode' => 'Code du site',
+));
+?>
